0f175d7a03b34dbe13d64383dcc73c4f694071ff
negin mohammadzadeh <34798457+neginmohammadzadeh@users.noreply.github.com>
2022-03-18 00:45:15 -0400

    Update productExceptSelf.java
    file edited

--- LeetCode/src/productExceptSelf.java
+++ LeetCode/src/productExceptSelf.java
@@ -4,13 +4,13 @@ class productExceptSelf {
     public static int[] findProductExceptSelf(int[] nums) {
         int[] res = new int[nums.length];
         res[0] = 1 ;
-        
+         // store the product upto i-1 index from the start of array
         for(int i=1 ; i<nums.length ; i++)
         {
             res[i] = res[i-1] * nums[i-1];
   
         }
-        
+        // store the product upto i+1 index from the end of array
         int temp = nums[nums.length - 1];
         for(int i = nums.length-2 ; i>=0 ;  i-- )
         {
@@ -22,13 +22,10 @@ class productExceptSelf {
     }
     
     public static void main(String[] args) {
-//		Scanner in = new Scanner(System.in);
     	
     	int arr[]= {1,2,3,4};
     	int res[]= findProductExceptSelf(arr);
-    	
-    	System.out.println(findProductExceptSelf(arr).toString());
-//		
+    			
 	}
     
 }
